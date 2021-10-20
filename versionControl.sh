273fe326e586f2c94e513be2ac770a5be2e9cab1
davidtopping02 <71976336+davidtopping02@users.noreply.github.com>
2021-10-20 11:44:07 +0100

    added function to checkin files

--- versionControl.sh
+++ versionControl.sh
@@ -1,14 +1,71 @@
 #! /bin/bash
 
-#add code
-checkout(){
+checkIn(){
+
+  #displaying all available respositories
+  echo "Which repository would you like to check a file into:"
+  ls workingDir
+
+  #getting user option of repository
+  repo="repoName"
+
+  #looping till valid repository is enterered
+  until [[ -d "workingDir/$repo" ]]; do
+
+    #getting repository name from user
+    read -p $'\nRepo: ' repo
+
+    #displaying message if the repository entered does not exist
+    if [[ ! -d "workingDir/$repo"  ]]; then
+      echo 'repository does not exist'
+    fi
+  done
+
+
+
+  #displaying all files that can be checked in
+  echo $'\nfiles available to check in'
+  ls workingDir/$repo
+
+  #init file to check in variable
+  fileToCheckIn="fileToCheckIn"
+
+  #looping till valid file is enterered
+  until [[ -f "workingDir/$repo/$fileToCheckIn" ]]; do
+
+    #getting user option of file to be checked in
+    read -p $'\nfile to check in: ' fileToCheckIn
+
+    #displaying message if the file entered does not exist
+    if [[ ! -f "workingDir/$repo/$fileToCheckIn"  ]]; then
+      echo 'file does not exist'
+    fi
+  done
+
+  #moving file back to the repository
+  mv workingDir/$repo/$fileToCheckIn repositories/$repo
+
+  #deleting folder if its the last file being checked in
+
+
+
+  #displaying message if move of directory worked
+  if [[ -f "repositories/$repo/$fileToCheckIn"  ]]; then
+    echo 'File checked in succesfully!'
+  else
+    echo "Error in checking in file '$fileToCheckIn'"
+  fi
+
+}
+
+
+checkOut(){
 
   #displaying all available respositories
   echo 'Respositories available:'
   ls repositories
 
   #getting user option of repository
-  # STR="Hello World!"
   repo="repoName"
 
   #looping till valid repository is enterered
@@ -18,40 +75,43 @@ checkout(){
     read -p $'\nRepo: ' repo
 
     #displaying message if the repository entered does not exist
-    if [[ ! -f "repositories/$repo"  ]]; then
+    if [[ ! -d "repositories/$repo"  ]]; then
       echo 'repository does not exist'
     fi
   done
 
-  fileToCheckOut="fileToCheckOut"
+  fileTo="fileTo"
 
   #displaying all files that can be checked out
   echo $'\nfiles available to check out'
   ls repositories/$repo
 
   #looping till valid file is enterered
-  until [[ -f "repositories/$repo/$fileToCheckOut" ]]; do
+  until [[ -f "repositories/$repo/$fileTo" ]]; do
 
-    #getting user option of file to be checkout
-    read -p $'\nfile to checkout: ' fileToCheckOut
+    #getting user option of file to be
+    read -p $'\nfile to : ' fileTo
 
     #displaying message if the file entered does not exist
-    if [[ ! -f "repositories/$repo/$fileToCheckOut"  ]]; then
+    if [[ ! -f "repositories/$repo/$fileTo"  ]]; then
       echo 'file does not exist'
     fi
   done
 
 
   #moving file to be checked out to working directory
-  mv repositories/$repo/$fileToCheckOut workingDir
+  if [[ ! -d workingDir/$repo ]]; then
+      mkdir workingDir/$repo
+  fi
+  mv repositories/$repo/$fileTo workingDir/$repo
 
   #displaying message if move of directory worked
-  if [[ -f "workingDir/$fileToCheckOut"  ]]; then
+  if [[ -f "workingDir/$repo/$fileTo"  ]]; then
     echo 'File checked out succesfully!'
   else
-    echo "Error in checking out file '$fileToCheckOut'"
+    echo "Error in checking out file '$fileTo'"
   fi
 
 }
 
-checkout
+checkIn
