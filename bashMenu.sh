7c6b7f92582b03cf9ed98d38187ebbb609435aff
Sandy <72385569+SN51-SYS@users.noreply.github.com>
2021-10-20 10:18:14 +0100

    Much improved menu system.

--- bashMenu.sh
+++ bashMenu.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo ""
+echo "-** Version Control Menu **-"
+echo ""
+echo "1: Create a new repository"
+echo "2: Add a file to the repository"
+echo "3: Edit or view a file in the repository"
+echo "0: Exit"
+
 function menuPrompt
 {	
+	echo ""
 	read -p "Please enter your choice: " userChoice
 
 	case $userChoice in
@@ -10,6 +19,10 @@ function menuPrompt
 		2 ) echo "Adding file to the repository..."
 		;;
 		3 ) echo "Checking out file for editing..."
+		;;
+		0 ) echo ""
+		echo "Thank you for using the program."
+		echo ""
 		;;
 		* ) echo "That is not a valid choice. Please try again."
 		menuPrompt
